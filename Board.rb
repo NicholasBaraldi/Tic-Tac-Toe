de0f336faba24e3d108fae5d80457411b7f2c05b
NicholasBaraldi <nicholas.baraldi@gmail.com>
2021-07-07 19:21:38 -0300

    added Board file

---
+++ Board.rb
@@ -0,0 +1,71 @@
+class Board
+
+    HEIGHT = 3
+    WIDTH = HEIGHT
+
+    attr_accessor :grid
+
+    def initialize
+        @grid = Array.new(HEIGHT) {Array.new(WIDTH, :" ")}
+    end
+
+    def print_grid
+        @grid.each do |row|
+            puts
+            row.each do |cell|
+                print "[#{cell}]"
+            end
+        end
+        puts
+    end
+
+    def row_win?(marker)
+        @grid.any? do |row|
+            row.all? do |cell|
+                cell == marker
+            end
+        end
+    end
+
+    def column_win?(marker)
+        (0...WIDTH).any? do |column|
+            @grid.all? do |row|
+                row[column] == marker
+            end
+        end
+    end
+
+    def diagonal_win?(marker)
+        [-1, 1].any? do |sign|
+            (0...HEIGHT).all? do |i|
+                second_index = 
+                    if sign > 0
+                        i
+                    else
+                        sign * (i+1)
+                    end
+                @grid[i][second_index] == marker
+            end
+        end
+    end
+
+    def [](y,x)
+        @grid[y][x]
+    end
+
+    def []=(y,x, marker)
+        if @grid[y][x] != :" " && [:X, :O].include?(marker)
+            false
+        else
+            @grid[y][x] = marker
+        end
+    end
+end
+
+b = Board.new
+
+b [0,2] = :x
+b [1,1] = :x
+b [2,0] = :x
+b.print_grid
+puts b.diagonal_win?(:x)
