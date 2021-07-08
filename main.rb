0377b9f3549d6b2b3076e934b50b7079e2455a51
NicholasBaraldi <nicholas.baraldi@gmail.com>
2021-07-07 23:41:38 -0300

    game init teste

--- main.rb
+++ main.rb
@@ -2,8 +2,13 @@ require_relative 'Game'
 require_relative 'Player'
 
 players = [
-    Player.new("Max", :X)
+    Player.new("Max", :X),
     Player.new("John", :O)
 ]
 
 game = Game.new(players)
+
+puts "Welcome to tic tac toe."
+puts "Player one, pick a move."
+game.turn(players[0])
+game.print_board
