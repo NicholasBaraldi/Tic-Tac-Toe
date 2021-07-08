d23baa0cbe37495a17304db7521b7ae546d6d42b
NicholasBaraldi <nicholas.baraldi@gmail.com>
2021-07-07 23:33:01 -0300

    defining turn method

--- Game.rb
+++ Game.rb
@@ -6,4 +6,14 @@ class Game
         @players = players
         @board = Board.new
     end
+
+    def turn(player)
+        move = player.get_move
+        @board[*move] = player.marker
+        @board.winner?(player.marker)
+    end
+
+    def print_board
+        @board.print_grid
+    end
 end
