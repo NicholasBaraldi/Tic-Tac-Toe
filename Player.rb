f0391c64b9b467683dfdae430015a801f2c640c2
NicholasBaraldi <nicholas.baraldi@gmail.com>
2021-07-08 02:41:04 -0300

    refactoring player input methods

--- Player.rb
+++ Player.rb
@@ -1,5 +1,7 @@
 class Player
 
+    ROW_MAP = (:A..:C).zip(0..2).to_h
+
     attr_reader :marker
 
     def initialize(name, marker)
@@ -9,11 +11,13 @@ class Player
     end
 
     def get_row
-        gets.to_i
+        print "> "
+        ROW_MAP[gets[0].upcase.to_sym]
     end
 
     def get_col
-        gets.to_i
+        print "> "
+        gets.to_i - 1
     end
 
     def get_move
