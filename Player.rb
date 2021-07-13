692c5bd04f5e8409ce16369c45f8e9cc4f43a285
NicholasBaraldi <nicholas.baraldi@gmail.com>
2021-07-13 18:38:26 -0300

    creating methods to validate user input

--- Player.rb
+++ Player.rb
@@ -10,17 +10,39 @@ class Player
         @score = 0
     end
 
+    def get_move
+        [get_row, get_col]
+    end
+
     def get_row
-        print "> "
-        ROW_MAP[gets[0].upcase.to_sym]
+        loop do
+            print "> "
+            input = gets
+            choice = format_row(input)
+            return choice if valid_choice?(choice);
+            puts "Invalid row."
+        end
     end
 
     def get_col
-        print "> "
-        gets.to_i - 1
+        loop do
+            print "> "
+            input = gets
+            choice = format_col(input)
+            return choice if valid_choice?(choice);
+            puts "Invalid column."
+        end
     end
 
-    def get_move
-        [get_row, get_col]
+    def format_row(input)
+        ROW_MAP[input[0].upcase.to_sym]
+    end
+
+    def format_col(input)
+        input.to_i - 1
+    end
+
+    def valid_choice?(choice)
+        (0..2).include?(choice)
     end
 end
